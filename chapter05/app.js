8e7891f33506ef2ead67dea1128169807885d932
changxianglin <281532612@qq.com>
2019-02-18 02:56:48 +0800

    application middleware

--- chapter05/app.js
+++ chapter05/app.js
@@ -2,20 +2,24 @@ const Koa = require('koa');
 const path = require('path');
 const bodyParser = require('koa-bodyparser');
 const nunjucks = require('koa-nunjucks-2');
+const staticFiles = require('koa-static');
 const app = new Koa();
 const router = require('./router');
+const middleware = require('./middleware')
 
+app.use(staticFiles(path.resolve(__dirname, "./public"), {
+    maxage: 30 * 24 * 60 * 60 * 1000
+}));    
 app.use(nunjucks({
     ext: 'html',
-    path: path.join(__dirname, 'views'),// 指定视图目录
+    path: path.join(__dirname, 'views'),
     nunjucksConfig: {
-        trimBlocks: true // 开启转义 防Xss
+        trimBlocks: true
     }
 }));
-
 app.use(bodyParser());
+middleware(app)
 router(app);
-
 app.listen(3000, () => {
     console.log('server is running at http://localhost:3000');
 });
